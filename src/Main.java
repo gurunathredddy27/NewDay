c9cfc87eaaa5dac9e2a7720aeb34248f18629d27
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2024-08-10 10:40:50 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[]args){
+        int a=20;
+        int b=108;
+
+        if(a>b)
+        System.out.println(a +" is greater than"+ b);
+        else{
+            System.out.println(a+ " is less than "+ b);
+        }
+    }
+}
